06e5fa7e52c3c41c172035a97f48cd96e1a81ad7
Daiki Yamashita <dik.piyo.piyo@gmail.com>
2017-06-21 18:30:23 +0900

    add settop function

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   if (request == "Action") {
-    func();
+    settop();
   }
 });
 
 function func() {
   alert("test");
+}
+
+function settop() {
+  chrome.app.window.get("main-view").setAlwaysOnTop(true);
 }
