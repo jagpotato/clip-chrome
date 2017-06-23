4b698228f28bc81a547114a54a5f4c3c839e7b48
Daiki Yamashita <dik.piyo.piyo@gmail.com>
2017-06-23 12:49:23 +0900

    create new panel

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.sendMessage(tab.id, "Action");
+  // chrome.tabs.sendMessage(tab.id, "Action");
+  chrome.windows.create({url: tab.url, type: "panel"}, function(window) {
+
+  });
 });
